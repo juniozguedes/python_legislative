e0e675b4038cf3781a59136458ea1f2694d35ea6
Wellington <juniozguedes@gmail.com>
2022-12-07 11:50:10 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+1. For every legislatorin the dataset, how many bills did the legislator support (voted forthe
+bill)? How many bills did the legislator oppose? ( legislators-support-oppose-count.csv)
+2. For every bill in the dataset, how many legislators supported the bill? How many legislators
+opposed the bill? Who was the primary sponsor of the bill?
+"""
+
+def legislator_supported_oppose():
+    return True
+    [{'id':'123', 'name':'John', 'approved_bills': 2, 'opposed_bills': 1}]
+
+def core():
+    print("Bills legislator supported and oppose")
+    legislator_supported_oppose()
+    return True
+
+if __name__ == "__main__":
+    print("Starting the program")
+    core()
