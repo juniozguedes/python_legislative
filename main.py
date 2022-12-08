5d82d1e24b40a5f9f62d345e09f8e64421171d93
Wellington <juniozguedes@gmail.com>
2022-12-08 11:30:19 -0300

    Finished legislator support and oppose

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ bill)? How many bills did the legislator oppose? ( legislators-support-oppose-co
 2. For every bill in the dataset, how many legislators supported the bill? How many legislators
 opposed the bill? Who was the primary sponsor of the bill?
 """
+import pandas as pd
+from IPython import display
 
 def legislator_supported_oppose():
-    return True
-    [{'id':'123', 'name':'John', 'approved_bills': 2, 'opposed_bills': 1}]
+    vote_results = pd.read_csv('vote_results.csv')
+    legislators = pd.read_csv('legislators.csv')
+    result_df = legislators.merge(vote_results[["legislator_id","vote_id","vote_type"]], left_on="id", right_on="legislator_id", how="left") \
+                       .groupby("id") \
+                       .apply(lambda x: pd.Series({
+                           "name": str(x["name"].iloc[0]),
+                           "num_supported_bills": len([v for v in x.vote_type if v==1]),
+                           "num_opposed_bills": len([v for v in x.vote_type if v==2]),
+                           })) \
+                       .reset_index()
+
+    print(result_df)
 
 def core():
     print("Bills legislator supported and oppose")
